3bc43116c87d798b8136939a38b09a78aae56d4e
do-bright <godqhrgotjs@gmail.com>
2022-02-04 18:19:00 +0900

    ~ 상품 서비스 개발

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional // 해당 어노테이션이 없으면 위에서 전체적으로 readOnly = true로 뒀기 때문에 수정이 불가능함
+    // 하지만 명시를 하면 false (원래 Transactional의 기본 값)으로 줄 수 있음
+    public void saveItem(Item item){
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems(){
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId){
+        return itemRepository.findOne(itemId);
+    }
+
+}

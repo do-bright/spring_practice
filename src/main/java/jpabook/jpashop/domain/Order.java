bd114863bb68bde81c0f5d466689d188ff59fe21
do-bright <godqhrgotjs@gmail.com>
2022-02-07 19:59:47 +0900

    ~ 주문, 주문상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -59,10 +60,54 @@ public class Order {
         delivery.setOrder(this);
     }
 
-
-
-
-
-
     // 주문 엔티티대로 작성 ; id member:Member orderItems:List delivery:Delivery orderDate:Date status:OrderStatus
+
+    // == 생성 매서드 == //
+    public static Order createdOrder(Member member, Delivery delivery, OrderItem... orderItems){ // ... = 여러개 넘긴다
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus((OrderStatus.ORDER)); // 주문한 상태로 만들기
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    // == 비즈니스 로직 == //
+    /**
+     * 주문 취소
+     */
+    public void cancel(){
+        if (delivery.getStatus() == DeliveryStatus.COMP){ // COMP 배송 완료라고 설정 했었음 == 배송 완료가 되면 취소를 못함
+            throw new IllegalStateException("이미 배송이 완료되어 상품 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL); // 상태 취소로 바꿔주기
+        for (OrderItem orderItem : orderItems){
+            // 한 번 주문할 때 고객이 상품을 여러개 주문 할 수 있음 -> OrderItem 여러개 생김 -> 주문 취소하면 각각에 cancel 날려주기 ! ; cancel이 할 것
+            orderItem.cancel(); // alt enter 해서 매소드 추가 해주기
+        }
+    }
+
+    // == 조회 로직 == //
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice(){
+        // alt enter 해서 매소드 추가 해주기
+        // getTotalPrice()로 해주는 이유 저기서 주문수량 * 주문 가격을 해줘야 하기 때문
+
+        return orderItems.stream()
+                .mapToInt(OrderItem::getTotalPrice)
+                .sum();
+//        아래 alt enter 해서 sum()으로 바꾸기
+
+//        int totalPrice=0;
+//        for (OrderItem orderItem : orderItems) {
+//            totalPrice += orderItem.getTotalPrice(); // alt enter 해서 매소드 추가 해주기
+//            // getTotalPrice()로 해주는 이유 저기서 주문수량 * 주문 가격을 해줘야 하기 때문
+//        }
+    }
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.domain;
 import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
 
@@ -26,7 +27,34 @@ public class OrderItem {
     private int orderPrice; //주문 가격
     private int count; //주문 수량
 
-
     // 주문상세 엔티티대로 작성 ; id item:Item order:Order orderPrice count
+
+    // == 생성 매서드 == //
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count); // 생성된 만큼 재고를 빼주기 (주문한 상품 만큼 재고 빼줘야 하니까)
+        return orderItem;
+    }
+
+    // == 비즈니스 로직 == //
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        getItem().addStock(count); // 재고를 다시 주문한 수량 만큼 올리기
+    }
+
+    // == 조회 로직 == //
+    /**
+     * 주문상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
+
 
 }
